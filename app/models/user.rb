f5e319f2ba4ecdec96943ca43a76fecdb88336cc
Naoki <st.ns.hp@gmail.com>
2020-02-01 07:45:47 +0000

    Add account activation all up

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,14 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
   
+  def activate
+      update_columns(activated: true, activated_at: Time.zone.now)
+    end
+    
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
   private
     
     def downcase_email
@@ -46,5 +54,4 @@ class User < ApplicationRecord
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
-
 end
