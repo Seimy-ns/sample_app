1a5938a54ebd9caff517f2f97417dc2397726241
Naoki <st.ns.hp@gmail.com>
2020-02-01 09:22:46 +0000

    Revise production.rb line 67

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = {host: https://6a18f57798344bbfae80ef72eb6aed96.vfs.cloud9.ap-northeast-1.amazonaws.com/}
+  config.action_mailer.default_url_options = {host: "https://6a18f57798344bbfae80ef72eb6aed96.vfs.cloud9.ap-northeast-1.amazonaws.com/"}
   ActionMailer::Base.smtp_settings ={
     :address => 'smtp.sendgrid.net',
     :port => '587',
