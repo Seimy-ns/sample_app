8e3090a4f545e9e4baf44f2e604b3b9e65b0f470
Naoki <st.ns.hp@gmail.com>
2020-02-01 09:01:00 +0000

    Revise production.rb line 69

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   host = '<your heroku app>.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings ={
-    :addresse => 'smtp.sendgrid.net',
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
